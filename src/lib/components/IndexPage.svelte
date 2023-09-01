75426dbf7130cfbfe417998b8772142a827d22b6
Suphakit P <me@suphakit.net>
2023-09-01 22:22:53 +0700

    chore<components>: added some animation

--- src/lib/components/IndexPage.svelte
+++ src/lib/components/IndexPage.svelte
@@ -5,8 +5,8 @@
 <section>
   <div class={`indexHeader w-full h-screen bg-mainBG text-whitesmoke flex flex-row justify-center items-center text-center`}>
     <div class="clipped p-2 w-[500px] bg-whitesmoke">
-      <div class={`clipped bg-[#ffb6bb] w-[100%] text-center py-4`}>
-        <p class="text-5xl">
+      <div class={`textHeader clipped bg-[#ffb6bb] w-[100%] text-center py-4`}>
+        <p class="text-5xl animate-pulse">
           Suphakit P.
         </p>
       </div>
