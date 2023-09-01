6766fb44466e06f6b28367dce2a3c1625cbbce82
Suphakit P <me@suphakit.net>
2023-09-01 11:13:20 +0700

    init<all>: project init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import './main.css';
+</script>
+
+<div class="bg-pastel-black text-whitesmoke min-h-screen w-full">
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+	
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'pastel-black': '#2e2f2f',
+			whitesmoke: '#f5f5f5',
+			'living-coral': '#fe6e61',
+			overlay: 'rgba(0,0,0,0.4)',
+			skyBlue: '#7ec4cf'
+		},
+		borderRadius: {
+			DEFAULT: '1rem'
+		},
+		extend: {
+			scale: {
+				85: '.85'
+			}
+		}
+	},
+	plugins: []
+};
