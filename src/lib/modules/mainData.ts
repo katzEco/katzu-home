5f6064bfce9ffd713f65fc3dc7f0a3afc781ceed
Suphakit P <me@suphakit.net>
2023-09-05 00:25:47 +0700

    feat<db>: added skill list

--- src/lib/modules/mainData.ts
+++ src/lib/modules/mainData.ts
@@ -41,6 +41,163 @@ const UserData = {
         startDate: "May 2021",
         endDate: "Present"
       }
+    ],
+    skills: [
+      {
+        img: "https://cordova.apache.org/",
+        link: "https://www.vectorlogo.zone/logos/apache_cordova/apache_cordova-icon.svg",
+        alt: "cordova"
+      },
+      {
+        img: "https://getbootstrap.com",
+        link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
+        alt: "bootstrap"
+      },
+      {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+        link: "https://www.cprogramming.co",
+        alt: "clang"
+      },
+      {
+        img: "https://raw.githubusercontent.com/Hardik0307/Hardik0307/master/assets/canvasjs-charts.svg",
+        link: "https://canvasjs.com",
+        alt: "canvasjs"
+      },
+      {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg",
+        link: "https://www.w3schools.com/cs/",
+        alt: "csharp"
+      },
+      {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+        link: "https://www.w3schools.com/css/",
+        alt: "css3"
+      },
+      {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+        link: "https://www.docker.com",
+        alt: "docker"
+      },
+      {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/electron/electron-original.svg",
+        link: "https://www.electronjs.org",
+        alt: "electron"
+      },
+      {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+        link: "https://expressjs.com",
+        alt: "express"
+      },
+      {
+        img: "https://www.vectorlogo.zone/logos/figma/figma-icon.svg",
+        link: "https://figma.com",
+        alt: "figma"
+      },
+      {
+        img: "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg",
+        link: "https://git-scm.com",
+        alt: "git"
+      },
+      {
+        img: "https://www.vectorlogo.zone/logos/hexoio/hexoio-icon.svg",
+        link: "https://hexo.io",
+        alt: "hexo"
+      },
+      {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+        link: "https://www.w3.org/html",
+        alt: "html5"
+      },
+      {
+        img: "https://www.vectorlogo.zone/logos/ifttt/ifttt-ar21.svg",
+        link: "https://ifttt.com",
+        alt: "ifttt"
+      },
+      {
+        img: "https://upload.wikimedia.org/wikipedia/commons/d/d1/Ionic_Logo.svg",
+        link: "https://ionicframework.com",
+        alt: "ionic"
+      },
+      {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+        link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+        alt: "js"
+      },
+      {
+        img: "https://www.vectorlogo.zone/logos/jekyllrb/jekyllrb-icon.svg",
+        link: "https://jekyllrb.co",
+        alt: "jekyll"
+      },
+      {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+        link: "https://www.mongodb.com/",
+        alt: "mongodb"
+      },
+      {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        link: "https://www.mysql.com",
+        alt: "mysql"
+      },
+      {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+        link: "https://nodejs.org",
+        alt: "nodejs"
+      },
+      {
+        img: "https://www.vectorlogo.zone/logos/nuxtjs/nuxtjs-icon.svg",
+        link: "https://nuxtjs.org/",
+        alt: "nuxtjs"
+      },
+      {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg",
+        link: "https://www.php.net",
+        alt: "php"
+      },
+      {
+        img: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+        link: "https://postman.com",
+        alt: "postman"
+      },
+      {
+        img: "https://cdn.worldvectorlogo.com/logos/pug.svg",
+        link: "https://pugjs.org",
+        alt: "pugjs"
+      },
+      {
+        img: "https://www.vectorlogo.zone/logos/pptrdev/pptrdev-official.svg",
+        link: "https://github.com/puppeteer/puppeteer",
+        alt: "puppeteer"
+      },
+      {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+        link: "https://reactjs.org",
+        alt: "reactjs"
+      },
+      {
+        img: "https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg",
+        link: "https://svelte.dev",
+        alt: "sveltejs"
+      },
+      {
+        img: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg",
+        link: "https://tailwindcss.com",
+        alt: "tailwind"
+      },
+      {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+        link: "https://www.typescriptlang.org",
+        alt: "typescript"
+      },
+      {
+        img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original-wordmark.svg",
+        link: "https://vuejs.org/",
+        alt: "vuejs"
+      },
+      {
+        img: "https://www.vectorlogo.zone/logos/zapier/zapier-icon.svg",
+        link: "https://zapier.com",
+        alt: "zapier"
+      }
     ]
   }
 }
