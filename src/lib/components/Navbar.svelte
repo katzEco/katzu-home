1b21675ad3426c6645ba1f3da4c22c747e7c79f9
Suphakit P <me@suphakit.net>
2023-09-06 08:56:28 +0700

    feat<components>: navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+
+  const Button = "p-4 hover:opacity-80 bg-mainBG hover:bg-pastel-black"
+  const ActiveButton = `${Button} !opacity-80 !bg-pastel-black`
+</script>
+
+<section class="w-full flex flex-row">
+  <div class="left w-full flex flex-row justify-start text-left">
+    <a href="/" class={Button}>
+      Suphakit P.
+    </a>
+  </div>
+  <div class="right w-full flex-row justify-end text-right py-4">
+    {#if $page.url.pathname == '/about'}
+      <!-- svelte-ignore a11y-invalid-attribute -->
+      <a href="#" class={ActiveButton}>
+        About
+      </a>
+    {:else}
+      <a href="/about" class={Button}>
+        About
+      </a>
+    {/if}
+
+    {#if $page.url.pathname == '/certs'}
+      <!-- svelte-ignore a11y-invalid-attribute -->
+      <a href="#" class={ActiveButton}>
+        Certificates
+      </a>
+    {:else}
+      <a href="/certs" class={Button}>
+        Certificates
+      </a>
+    {/if}
+
+    {#if $page.url.pathname == '/commission'}
+      <!-- svelte-ignore a11y-invalid-attribute -->
+      <a href="#" class={ActiveButton}>
+        Commission
+      </a>
+    {:else}
+      <a href="/Commission" class={Button}>
+        Certificates
+      </a>
+    {/if}
+
+    {#if $page.url.pathname == '/contact'}
+      <!-- svelte-ignore a11y-invalid-attribute -->
+      <a href="#" class={ActiveButton}>
+        Contact
+      </a>
+    {:else}
+      <a href="/contact" class={Button}>
+        Contact
+      </a>
+    {/if}
+  </div>
+</section>
