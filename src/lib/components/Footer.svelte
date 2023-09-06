544db0c2a81f360b02291ad14c24f3a51a6900ca
Suphakit P <me@suphakit.net>
2023-09-06 13:59:15 +0700

    chore<component>: added footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,7 @@
+<script>
+	import TW_CLASS from "$lib/constants/tailwind";
+</script>
+
+<div class="w-full p-4 text-center bg-ctp-base text-ctp-text opacity-80">
+  &copy; 2023 <a href="https://detzz.in.th" target="_blank" class={TW_CLASS.link}>Suphakit P.</a> All right reserved.
+</div>
