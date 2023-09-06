5c7669e2b6ca3ee8fcff5f32beab68a643b07710
Suphakit P <me@suphakit.net>
2023-09-06 10:01:20 +0700

    chore<pages>: fix file routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import IndexPage from "$lib/components/IndexPage.svelte";
+	import Index from "$lib/components/Pages/Index.svelte";
   import Loading from "$lib/components/Loading.svelte";
 	import PreLoad from "$lib/components/PreLoad.svelte";
 
@@ -28,6 +28,6 @@
     <Loading opacityClass="loadOut opacity-0" />
     <PreLoad/>
   {:else}
-    <IndexPage />
+    <Index />
   {/if}
 {/if}
