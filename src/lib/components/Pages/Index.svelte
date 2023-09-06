f1e5c7c7b1fdbc77c664d77015365fb9b2599ec1
Suphakit P <me@suphakit.net>
2023-09-06 14:18:55 +0700

    fix<page>: color scheme

--- src/lib/components/Pages/Index.svelte
+++ src/lib/components/Pages/Index.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import data from "$lib/modules/mainData"
 
-  const Button = "w-[40%] md:w-full p-4 bg-ctp-maroon text-ctp-base rounded duration-300 shadow-lg shadow-[grey] hover:shadow-sm hover:opacity-60 hover:scale-90"
+  const Button = "w-[40%] md:w-full p-4 bg-ctp-sapphire text-ctp-base rounded duration-300 shadow-lg shadow-[grey] hover:shadow-sm hover:opacity-60 hover:scale-90"
 </script>
 
 <section>
   <div class={`indexHeader w-full h-screen bg-ctp-base text-ctp-text flex flex-col justify-center items-center text-center gap-8`}>
-    <div class="heading-holder p-4 rounded bg-ctp-text flex flex-col gap-2 shadow-lg shadow-ctp-text hover:shadow-ctp-maroon hover:border-ctp-maroon hover:border-solid border-ctp-text border-2">
+    <div class="heading-holder p-4 rounded bg-ctp-text flex flex-col gap-2 shadow-lg shadow-ctp-text hover:shadow-ctp-sapphire hover:border-ctp-sapphire hover:border-solid border-ctp-text border-2">
       <div class="!justify-end !text-right w-full">
-        <img src={data.image} alt="profile" class="w-[100px] h-auto rounded-[100%] ml-auto border-2 border-ctp-maroon border-solid p-1">
+        <img src={data.image} alt="profile" class="w-[100px] h-auto rounded-[100%] ml-auto border-4 border-ctp-sapphire border-solid p-1">
       </div>
       <div class="!text-left">
         <p class="text-4xl py-2 text-pastel-black">
